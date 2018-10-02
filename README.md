021b8dcfa398b43b0b581c6269b9bd45ffac1a08
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-10-02 18:59:38 +0300

    Add Kovenant

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # SampleKovenant
 Using Kovenant in Android
+
+## Requirements
+
+* Android Studio 3.3 Canary 12
+* Kotlin 1.3.0-rc-116
+* Android Gradle Plugin 3.3.0-alpha12
+* Gradle wrapper 4.10.1
+* AAPT 2
+
+## References
+http://kovenant.komponents.nl/
+
+https://github.com/mplatvoet/kovenant
+
+https://www.baeldung.com/kotlin-kovenant
--- app/src/main/java/com/gpetuhov/android/samplekovenant/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplekovenant/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.gpetuhov.android.samplekovenant
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import nl.komponents.kovenant.task
+import nl.komponents.kovenant.then
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "Kovenant"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView.setOnClickListener {
+            task {
+                Thread.sleep(1000)
+                1 + 1
+            } then {
+                Thread.sleep(1000)
+                it * 2
+            } success {
+                Log.d(TAG, "Result is $it")
+            } fail {
+                Log.d(TAG, it.message)
+            } always {
+                Log.d(TAG, "End of task")
+                runOnUiThread { toast("Task complete") }
+            }
+        }
     }
 }
