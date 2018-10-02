7804af7dd642bfcc79493febd5433fbeb70f5fd8
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-10-02 19:01:45 +0300

    Add comments

--- app/src/main/java/com/gpetuhov/android/samplekovenant/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplekovenant/MainActivity.kt
@@ -17,18 +17,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         textView.setOnClickListener {
+            // This runs in background
             task {
                 Thread.sleep(1000)
                 1 + 1
+            // Tasks can be chained
             } then {
                 Thread.sleep(1000)
                 it * 2
+            // This runs on success
             } success {
                 Log.d(TAG, "Result is $it")
+            // This runs on error
             } fail {
                 Log.d(TAG, it.message)
+            // This runs both on success and on error
             } always {
                 Log.d(TAG, "End of task")
+                // This is needed to show toast on main thread
                 runOnUiThread { toast("Task complete") }
             }
         }
